47db60c33c5addb36c436ca2ae6bf169813d0f84
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-17 08:22:01 +0500

    5

--- average.py
+++ average.py
@@ -10,7 +10,15 @@ def average_expenses(file_path: str) -> float:
     Returns:
         float: average expenses
     """
-    pass
+    f=open('data.json')
+    x=json.load(f)
+    a=0
+    b=0
+    for i in x.values():
+        b+=i
+        a+=i
+    return b/a
+
 
 # test
 file_path = "data.json"
