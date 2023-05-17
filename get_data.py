20c1e467e2c0096af84d5573b007e907f4740652
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-17 08:02:19 +0500

    1

--- get_data.py
+++ get_data.py
@@ -10,9 +10,9 @@ def get_data(file_path: str) -> dict:
     Returns:
         dict: dictionary of data
     """
-    pass
-
-
+    f=open('data.json')
+    x=json.load(f)
+    return x
 # test
 file_path = "data.json"
 data = get_data(file_path)
